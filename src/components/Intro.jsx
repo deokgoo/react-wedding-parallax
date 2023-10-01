a45f66108323efba103a791ea46433080c396f1d
deokgoo <deokgoo.kima@oliveyoung.co.kr>
2023-10-02 00:40:38 +0900

    feature: 인트로페이지 애니메이션 효과 추가

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,13 @@
+
 import styles from '../style/Intro.module.scss';
+import Particle from './Particle';
 
 const Intro = () => {
   return (
     <div className={styles.introContainer}>
       <div className={styles.backgroundImg}>
       </div>
+      <Particle />
     </div>
   );
 }
