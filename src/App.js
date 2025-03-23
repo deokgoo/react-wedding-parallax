bdededaccb5e51e69d42cf417d0681d621af6e36
deokgoo <kkddgg1001@gmail.com>
2025-03-23 10:16:19 +0900

    fix: naver map 수정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { NavermapsProvider } from 'react-naver-maps';
 import './style/global.scss';
 
 function App() {
-  const naverMapClientId = 'tw4a68fkng';
+  const naverMapClientId = '6ygkj3aqk9';
 
   return (
     <NavermapsProvider
